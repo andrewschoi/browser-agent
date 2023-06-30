1a106d9ad567108c7bfa59e4f454de2b791a8940
andrew <asc269@cornell.edu>
2023-06-29 20:10:15 -0500

    read html files into test_maker

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# browser-agent
+# Browser agent
--- html_parser/test/test_maker.py
+++ html_parser/test/test_maker.py
@@ -1,8 +1,16 @@
 from html_parser.context_maker import maker
 import unittest
 
+
+with open('html_parser/test/html_samples/form.html', 'r') as f:
+  form = f.read()
+
+with open('html_parser/test/html_samples/story.html', 'r') as f:
+  story = f.read()
+
+
 class TestMaker(unittest.TestCase):
   def test_simple(self):
     pass
-  
+
 unittest.main()
