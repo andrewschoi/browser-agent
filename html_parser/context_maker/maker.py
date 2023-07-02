dac55cd97e322123978fa3f6110a55aaa0ef28a1
andrew <asc269@cornell.edu>
2023-07-01 19:48:55 -0500

    formatting

--- html_parser/context_maker/maker.py
+++ html_parser/context_maker/maker.py
@@ -3,80 +3,79 @@ from bs4.element import Tag
 import json
 import collections
 
-class Context():
-  """
-  A relational representation of html context.
-  - html: the associated html string
-  - children[<tag>]: a list of tags that are nested within <tag>
-  - siblings[<tag>]: a list of tags that are on the same level as <tag>
-  - parents[<tag>]: an ordered list of tages that contain <tag> as a descendant
-  """
-  def __init__(self, html, children=[], siblings=[], parents=[]):
-    self._html = html
-    self._children = children
-    self._siblings = siblings
-    self._parents = parents
-  
-  @property
-  def parents(self):
-    return self._parents
-  
-  @property
-  def siblings(self):
-    return self._siblings
-  
-  @property
-  def children(self):
-    return self._children
-  
-  @property
-  def html(self):
-    return self._html
 
-  def __str__(self):
-    return json.dumps({
-      "children": self.children,
-      "siblings": self.siblings,
-      "parents": self.parents
-    })
+class Context:
+    """
+    A relational representation of html context.
+    - html: the associated html string
+    - children[<tag>]: a list of tags that are nested within <tag>
+    - siblings[<tag>]: a list of tags that are on the same level as <tag>
+    - parents[<tag>]: an ordered list of tages that contain <tag> as a descendant
+    """
 
+    def __init__(self, html, children=[], siblings=[], parents=[]):
+        self._html = html
+        self._children = children
+        self._siblings = siblings
+        self._parents = parents
 
-class ContextBuilder():
-  """
-  Builds a [Context] based on html string [html]
+    @property
+    def parents(self):
+        return self._parents
 
-  Example usage:
-    some_html_string = <h1>hello world<h1>
-    some_context = ContextBuilder(some_html_string).build()
-  """
-  def __init__(self, html):
-    self.html = html
-    self.siblings = collections.defaultdict(list)
-    self.children =  collections.defaultdict(list)
-    self.parents = collections.defaultdict(list)
-  
+    @property
+    def siblings(self):
+        return self._siblings
 
-  def build(self):
-    soup = BeautifulSoup(self.html, "html.parser")
-    
-    for tag in soup.findAll():
-      if isinstance(tag, Tag):
-        self.children[tag] = list(tag.descendants)
-        self.siblings[tag] = list(tag.next_siblings)
-      else:
-        self.children[tag] = []
-        self.siblings[tag] = []
+    @property
+    def children(self):
+        return self._children
 
-      self.parents[tag] = tag.parents 
-    
+    @property
+    def html(self):
+        return self._html
 
-    return Context(self.html, children=self.children, siblings=self.siblings, parents=self.parents)
-
-    
-    
-      
+    def __str__(self):
+        return json.dumps(
+            {
+                "children": self.children,
+                "siblings": self.siblings,
+                "parents": self.parents,
+            }
+        )
 
 
+class ContextBuilder:
+    """
+    Builds a [Context] based on html string [html]
 
+    Example usage:
+      some_html_string = <h1>hello world<h1>
+      some_context = ContextBuilder(some_html_string).build()
+    """
 
+    def __init__(self, html):
+        self.html = html
+        self.siblings = collections.defaultdict(list)
+        self.children = collections.defaultdict(list)
+        self.parents = collections.defaultdict(list)
 
+    def build(self):
+        soup = BeautifulSoup(self.html, "html.parser")
+
+        for tag in soup.findAll():
+            if isinstance(tag, Tag):
+                self.children[tag] = list(tag.descendants)
+                self.siblings[tag] = list(tag.next_siblings)
+            else:
+                self.children[tag] = []
+                self.siblings[tag] = []
+
+            self.parents[tag] = tag.parents
+
+        return Context(
+            self.html,
+            children=self.children,
+            siblings=self.siblings,
+            parents=self.parents,
+        )
--- html_parser/test/test_maker.py
+++ html_parser/test/test_maker.py
@@ -4,51 +4,49 @@ import unittest
 import collections
 
 
-with open('html_parser/test/html_samples/form.html', 'r') as f:
-  form = f.read()
+with open("html_parser/test/html_samples/form.html", "r") as f:
+    form = f.read()
 
-with open('html_parser/test/html_samples/story.html', 'r') as f:
-  story = f.read()
+with open("html_parser/test/html_samples/story.html", "r") as f:
+    story = f.read()
 
 
 class TestMaker(unittest.TestCase):
-  def test_build_successful(self):
-    story_context = ContextBuilder(story).build()
-    form_context = ContextBuilder(form).build()    
+    def test_build_successful(self):
+        story_context = ContextBuilder(story).build()
+        form_context = ContextBuilder(form).build()
 
+    def test_story_relations(self):
+        soup = BeautifulSoup(story, "html.parser")
+        story_context = ContextBuilder(story).build()
 
-  def test_story_relations(self):
-    soup = BeautifulSoup(story, 'html.parser')
-    story_context = ContextBuilder(story).build()
+        story_children = story_context.children
+        correct_children = collections.defaultdict(list)
 
-    story_children = story_context.children
-    correct_children = collections.defaultdict(list)
+        for tag in soup.findAll():
+            correct_children[tag] = list(tag.descendants)
 
-    for tag in soup.findAll():
-      correct_children[tag] = list(tag.descendants)
-    
-    self.assertEqual(len(story_children.keys()), len(correct_children.keys()))
-    for k in correct_children.keys():
-      self.assertEqual(set(story_children[k]), set(correct_children[k]))
-    
-    story_siblings = story_context.siblings
-    correct_siblings = collections.defaultdict(list)
-    for tag in soup.findAll():
-      correct_siblings[tag] = list(tag.next_siblings)
-    
-    self.assertEqual(len(story_siblings.keys()), len(correct_siblings.keys()))
-    for k in correct_siblings.keys():
-      self.assertEqual(set(story_siblings[k]), set(correct_siblings[k]))
+        self.assertEqual(len(story_children.keys()), len(correct_children.keys()))
+        for k in correct_children.keys():
+            self.assertEqual(set(story_children[k]), set(correct_children[k]))
 
-    
-    story_parents = story_context.parents
-    correct_parents = collections.defaultdict(list)
-    for tag in soup.findAll():
-      correct_parents[tag] = list(tag.parents)
+        story_siblings = story_context.siblings
+        correct_siblings = collections.defaultdict(list)
+        for tag in soup.findAll():
+            correct_siblings[tag] = list(tag.next_siblings)
 
-    self.assertEqual(len(story_parents), len(correct_parents))
-    for k in correct_parents.keys():
-      self.assertEqual(set(story_parents[k]), set(correct_parents[k]))
-      
+        self.assertEqual(len(story_siblings.keys()), len(correct_siblings.keys()))
+        for k in correct_siblings.keys():
+            self.assertEqual(set(story_siblings[k]), set(correct_siblings[k]))
+
+        story_parents = story_context.parents
+        correct_parents = collections.defaultdict(list)
+        for tag in soup.findAll():
+            correct_parents[tag] = list(tag.parents)
+
+        self.assertEqual(len(story_parents), len(correct_parents))
+        for k in correct_parents.keys():
+            self.assertEqual(set(story_parents[k]), set(correct_parents[k]))
+
 
 unittest.main()
