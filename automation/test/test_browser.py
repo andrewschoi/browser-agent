fbb9e07348458ece1aa0e5fd7a197c667c9c5ebd
andrew <asc269@cornell.edu>
2023-07-05 11:02:23 -0500

    add test_browser module

---
+++ automation/test/test_browser.py
@@ -0,0 +1,24 @@
+from automation.browser.browser import BrowserSingleton
+import unittest
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+
+class TestBrowser(unittest.TestCase):
+    def test_build_successful(self):
+        try:
+            options = Options()
+            options.headless = True
+            driver = webdriver.Firefox(options=options)
+            browser = BrowserSingleton(driver, None)
+            browser.go("https://github.com/")
+            browser.html()
+        except Exception as e:
+            print(e)
+            driver.close()
+            raise Exception
+        finally:
+            driver.close()
+
+
+unittest.main()
